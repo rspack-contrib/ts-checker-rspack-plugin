3e483d4b7edd374ec14c72480937930db14ab920
neverland <chenjiahan.jait@bytedance.com>
2025-05-15 13:05:50 +0800

    fix: append star to improve ignore patterns (#23)

--- test/unit/watch/inclusive-node-watch-file-system.test.ts
+++ test/unit/watch/inclusive-node-watch-file-system.test.ts
@@ -6,6 +6,14 @@ describe('createIsIgnored', () => {
   const distModulePath = '/path/to/dist/foo.js';
   const srcModulePath = '/path/to/src/foo.ts';
 
+  it('should handle built-in ignored directories', () => {
+    const customPath = '/path/without/git/or/node_modules';
+    const isIgnored = createIsIgnored([], []);
+    expect(isIgnored(gitPath)).toBe(true);
+    expect(isIgnored(customPath)).toBe(false);
+    expect(isIgnored('/path/to/.github/foo')).toBe(false);
+  });
+
   it('should allow to passing RegExp to the first argument', () => {
     const isIgnored = createIsIgnored([/[\\/](?:\.git|node_modules)[\\/]/], []);
     expect(isIgnored(gitPath)).toBe(true);
@@ -52,5 +60,34 @@ describe('createIsIgnored', () => {
     expect(isIgnored3(nodeModulesPath)).toBe(true);
     expect(isIgnored3(distModulePath)).toBe(true);
     expect(isIgnored3(srcModulePath)).toBe(false);
+
+    // exclude: ["**/dist"] in tsconfig.json
+    const isIgnored4 = createIsIgnored([], ['/path/to/**/dist']);
+    expect(isIgnored4(gitPath)).toBe(true);
+    expect(isIgnored4(nodeModulesPath)).toBe(true);
+    expect(isIgnored4(distModulePath)).toBe(true);
+    expect(isIgnored4(srcModulePath)).toBe(false);
+
+    // exclude: ["**/dist/"] in tsconfig.json
+    const isIgnored5 = createIsIgnored([], ['/path/to/**/dist/']);
+    expect(isIgnored5(gitPath)).toBe(true);
+    expect(isIgnored5(nodeModulesPath)).toBe(true);
+    expect(isIgnored5(distModulePath)).toBe(true);
+    expect(isIgnored5(srcModulePath)).toBe(false);
+
+    // exclude: ["dist/**/*"] in tsconfig.json
+    const isIgnored6 = createIsIgnored([], ['/path/to/dist/**/*']);
+    expect(isIgnored6(gitPath)).toBe(true);
+    expect(isIgnored6(nodeModulesPath)).toBe(false);
+    expect(isIgnored6(distModulePath)).toBe(true);
+    expect(isIgnored6(srcModulePath)).toBe(false);
+  });
+
+  it('should combine multiple ignore patterns', () => {
+    const isIgnored = createIsIgnored([/[\\/]node_modules[\\/]/], ['/path/to/dist']);
+    expect(isIgnored(gitPath)).toBe(true);
+    expect(isIgnored(nodeModulesPath)).toBe(true);
+    expect(isIgnored(distModulePath)).toBe(true);
+    expect(isIgnored(srcModulePath)).toBe(false);
   });
 });
