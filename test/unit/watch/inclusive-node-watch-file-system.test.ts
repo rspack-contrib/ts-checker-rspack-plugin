d6e494ac7afd0e5a44bece41539bad22d9bfecbc
neverland <chenjiahan.jait@bytedance.com>
2025-05-15 11:36:51 +0800

    fix: glob pattern in tsconfig.json#exclude not work

--- test/unit/watch/inclusive-node-watch-file-system.test.ts
+++ test/unit/watch/inclusive-node-watch-file-system.test.ts
@@ -23,10 +23,34 @@ describe('createIsIgnored', () => {
   });
 
   it('should allow to passing string path to the second argument', () => {
+    // exclude: ["dist"] in tsconfig.json
     const isIgnored = createIsIgnored([], ['/path/to/dist']);
     expect(isIgnored(gitPath)).toBe(true);
     expect(isIgnored(nodeModulesPath)).toBe(false);
     expect(isIgnored(distModulePath)).toBe(true);
     expect(isIgnored(srcModulePath)).toBe(false);
+  });
+
+  it('should allow to passing glob path to the second argument', () => {
+    // exclude: ["dist/**"] in tsconfig.json
+    const isIgnored1 = createIsIgnored([], ['/path/to/dist/**']);
+    expect(isIgnored1(gitPath)).toBe(true);
+    expect(isIgnored1(nodeModulesPath)).toBe(false);
+    expect(isIgnored1(distModulePath)).toBe(true);
+    expect(isIgnored1(srcModulePath)).toBe(false);
+
+    // exclude: ["**/dist/**"] in tsconfig.json
+    const isIgnored2 = createIsIgnored([], ['/path/to/**/dist/**']);
+    expect(isIgnored2(gitPath)).toBe(true);
+    expect(isIgnored2(nodeModulesPath)).toBe(true);
+    expect(isIgnored2(distModulePath)).toBe(true);
+    expect(isIgnored2(srcModulePath)).toBe(false);
+
+    // exclude: ["**/dist/*"] in tsconfig.json
+    const isIgnored3 = createIsIgnored([], ['/path/to/**/dist/*']);
+    expect(isIgnored3(gitPath)).toBe(true);
+    expect(isIgnored3(nodeModulesPath)).toBe(true);
+    expect(isIgnored3(distModulePath)).toBe(true);
+    expect(isIgnored3(srcModulePath)).toBe(false);
   });
 });
