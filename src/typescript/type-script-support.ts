9218a1e28877226c5f6b44a4b7af0d7cd39ed5d2
neverland <chenjiahan.jait@bytedance.com>
2025-12-18 11:15:57 +0800

    fix: replace webpack references with Rspack (#56)

--- src/typescript/type-script-support.ts
+++ src/typescript/type-script-support.ts
@@ -9,7 +9,7 @@ function assertTypeScriptSupport(config: TypeScriptWorkerConfig) {
   try {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     typescriptVersion = require(config.typescriptPath).version;
-  } catch (error) {
+  } catch {
     // silent catch
   }
 
@@ -23,9 +23,9 @@ function assertTypeScriptSupport(config: TypeScriptWorkerConfig) {
     throw new Error(
       [
         `Cannot find the "${config.configFile}" file.`,
-        `Please check webpack and TsCheckerRspackPlugin configuration.`,
+        `Please check Rspack and TsCheckerRspackPlugin configuration.`,
         `Possible errors:`,
-        '  - wrong `context` directory in webpack configuration (if `configFile` is not set or is a relative path in the fork plugin configuration)',
+        '  - wrong `context` directory in Rspack configuration (if `configFile` is not set or is a relative path in the fork plugin configuration)',
         '  - wrong `typescript.configFile` path in the plugin configuration (should be a relative or absolute path)',
       ].join(os.EOL)
     );
